e12da435208062c29c6e1d3728092ae26005f236
JayKon17 <jonwrigley@gmail.com>
2025-02-17 02:34:52 -0500

    Massive Overhaul

--- server/server.py
+++ server/server.py
@@ -1,7 +1,11 @@
 import http.server
 import socketserver
+import os
 
 PORT = 8000
+
+# Change the directory to one level up
+os.chdir('..')  # Go up one directory from the current directory
 
 Handler = http.server.SimpleHTTPRequestHandler
 
